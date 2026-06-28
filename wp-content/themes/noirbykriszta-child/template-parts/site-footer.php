f5e25326bfbaa1a68ecff276b41f85308798775b
viziadam <113590667+viziadam@users.noreply.github.com>
2026-06-28 12:46:20 +0200

    Add modular site footer template part

---
+++ wp-content/themes/noirbykriszta-child/template-parts/site-footer.php
@@ -0,0 +1,4 @@
+<?php
+if (!defined('ABSPATH')) exit;
+?>
+<footer class="noir-site-footer"><div class="noir-site-footer__inner"><strong>Noir by Kriszta</strong><span>Szempilla és szemöldök szolgáltatások</span></div></footer>
